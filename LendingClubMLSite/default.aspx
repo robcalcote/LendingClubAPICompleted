1185f99c90cf3724482461d0da77491f3545a670
Rob <robcalcote@gmail.com>
2019-02-20 18:47:35 -0700

    Finished API assignment and published!

--- LendingClubMLSite/default.aspx
+++ LendingClubMLSite/default.aspx
@@ -15,10 +15,114 @@
             <h1>Rob Calcote Lending Club Prediction Calculator</h1>
         </div>
 
+        <table>
+            <tr><td>Loan Purpose:</td>
+                <td><asp:DropDownList ID="ddlLoanPurpose" runat="server">
+                    <asp:ListItem></asp:ListItem>
+                    <asp:ListItem Value="debt_consolidation">Debt Consolidation</asp:ListItem>
+                    <asp:ListItem Value="credit_card">Credit Card</asp:ListItem>
+                    <asp:ListItem Value="major_purchase">Major Purchase</asp:ListItem>
+                    <asp:ListItem Value="small_business">Small Business</asp:ListItem>
+                    <asp:ListItem Value="car">Car</asp:ListItem>
+                    <asp:ListItem Value="home_improvement">Home Improvement</asp:ListItem>
+                    <asp:ListItem Value="house">House</asp:ListItem>
+                    <asp:ListItem Value="medical">Medical</asp:ListItem>
+                    <asp:ListItem Value="moving">Moving</asp:ListItem>
+                    <asp:ListItem Value="renewable_energy">Renewable Energy</asp:ListItem>
+                    <asp:ListItem Value="vacation">Vacation</asp:ListItem>
+                    <asp:ListItem Value="other">Other</asp:ListItem>
+                </asp:DropDownList></td></tr>
+            <tr><td>Employment Length:</td>
+                <td><asp:DropDownList ID="ddlEmploymentLength" runat="server">
+                    <asp:ListItem></asp:ListItem>
+                    <asp:ListItem>&lt; 1 year</asp:ListItem>
+                    <asp:ListItem>1 year</asp:ListItem>
+                    <asp:ListItem>2 years</asp:ListItem>
+                    <asp:ListItem>3 years</asp:ListItem>
+                    <asp:ListItem>4 years</asp:ListItem>
+                    <asp:ListItem>5 years</asp:ListItem>
+                    <asp:ListItem>6 years</asp:ListItem>
+                    <asp:ListItem>7 years</asp:ListItem>
+                    <asp:ListItem>8 years</asp:ListItem>
+                    <asp:ListItem>9 years</asp:ListItem>
+                    <asp:ListItem>10+ years</asp:ListItem>
+                </asp:DropDownList></td></tr>
+            <tr><td>Home Ownership:</td>
+                <td><asp:RadioButtonList ID="rblHomeOwnership" runat="server">
+                    <asp:ListItem Value="MORTGAGE">Mortgage</asp:ListItem>
+                    <asp:ListItem Value="RENT">Rent</asp:ListItem>
+                    <asp:ListItem Value="OWN">Own</asp:ListItem>
+                </asp:RadioButtonList></td></tr>
+            <tr><td>Annual Income:</td>
+                <td><asp:TextBox ID="txtIncome" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Income Verification:</td>
+                <td><asp:RadioButtonList ID="rblIncVerification" runat="server">
+                    <asp:ListItem>Source Verified</asp:ListItem>
+                    <asp:ListItem>Verified</asp:ListItem>
+                    <asp:ListItem>Not Verified</asp:ListItem>
+                </asp:RadioButtonList></td></tr>
+            <tr><td>Debt-to-Income Ratio (dti):</td>
+                <td><asp:TextBox ID="txtDTI" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Past Delinquencies (2 years):</td>
+                <td><asp:TextBox ID="txtPastDelinquencies" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Credit Report age in days:</td>
+                <td><asp:TextBox ID="txtCreditAgeDays" runat="server"></asp:TextBox></td></tr>
+            <tr><td>FICO Score:</td>
+                <td><asp:TextBox ID="txtFICO" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Past Inquiries (6 months):</td>
+                <td><asp:TextBox ID="txtInquiries6" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Months since delinquency:</td>
+                <td><asp:TextBox ID="txtLastDelinquency" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Open Credit Accounts:</td>
+                <td><asp:TextBox ID="txtOpenAccounts" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Number of Derogatory Records:</td>
+                <td><asp:TextBox ID="txtDerogatoryRecords" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Total Credit Balance:</td>
+                <td><asp:TextBox ID="txtTotalCreditBalance" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Rate of Credit Utilization (%):</td>
+                <td><asp:TextBox ID="txtCreditUtil" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Total Accounts on Credit:</td>
+                <td><asp:TextBox ID="txtTotAccounts" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Total Past Collections (12 months)</td>
+                <td><asp:TextBox ID="txtPastCollections12" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Delinquent Accounts:</td>
+                <td><asp:TextBox ID="txtDelinquentAmount" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Total Current Balance (All Accounts):</td>
+                <td><asp:TextBox ID="txtTotCurBalance" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Revolving Trades Opened (24 months):</td>
+                <td><asp:TextBox ID="txtRevTrades24" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Balance to Credit Limit:</td>
+                <td><asp:TextBox ID="txtBalanceToLimit" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Past Inquiries (12 months):</td>
+                <td><asp:TextBox ID="txtInquiries12" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Accounts opened (24 months):</td>
+                <td><asp:TextBox ID="txtAccountsOpened24" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Past Charge-Offs (12 months):</td>
+                <td><asp:TextBox ID="txtChargeOffs" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Past Due Total Amount:</td>
+                <td><asp:TextBox ID="txtPastDueAmount" runat="server"></asp:TextBox></td></tr>
+            <tr><td>Mortgage Accounts:</td>
+                <td><asp:RadioButtonList ID="txtMortgage" runat="server">
+                    <asp:ListItem Value="0">No</asp:ListItem>
+                    <asp:ListItem Value="1">Yes</asp:ListItem>
+                </asp:RadioButtonList></td></tr>
+            <tr><td>Public Record Bankruptcies:</td>
+                <td><asp:RadioButtonList ID="txtBankruptcies" runat="server">
+                    <asp:ListItem Value="0">No</asp:ListItem>
+                    <asp:ListItem Value="1">Yes</asp:ListItem>
+                </asp:RadioButtonList></td></tr>
+            <tr><td>Tax Liens:</td>
+                <td><asp:RadioButtonList ID="txtTaxLiens" runat="server">
+                    <asp:ListItem Value="0">No</asp:ListItem>
+                    <asp:ListItem Value="1">Yes</asp:ListItem>
+                </asp:RadioButtonList></td></tr>
+            <tr><td></td><td><asp:LinkButton ID="lbPredict" runat="server" OnClick="lbPredict_Click">Predict</asp:LinkButton></td></tr>
+        </table>
+        <br />
 
-        <asp:LinkButton ID="lbPredict" runat="server" OnClick="lbPredict_Click">Predict</asp:LinkButton>
-        <asp:Label ID="lblOutput" runat="server" Text="{Prediction Displays Here}"></asp:Label>
-
+        <!-- Output Tag -->
+        <h2><asp:Label ID="lblOutput" runat="server" Text="{Prediction Displays Here}"></asp:Label></h2>
+        <br />
     </form>
 </body>
 </html>
--- LendingClubMLSite/default.aspx.designer.cs
+++ LendingClubMLSite/default.aspx.designer.cs
@@ -22,6 +22,258 @@ namespace LendingClubMLSite {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// ddlLoanPurpose control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlLoanPurpose;
+        
+        /// <summary>
+        /// ddlEmploymentLength control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlEmploymentLength;
+        
+        /// <summary>
+        /// rblHomeOwnership control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList rblHomeOwnership;
+        
+        /// <summary>
+        /// txtIncome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtIncome;
+        
+        /// <summary>
+        /// rblIncVerification control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList rblIncVerification;
+        
+        /// <summary>
+        /// txtDTI control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDTI;
+        
+        /// <summary>
+        /// txtPastDelinquencies control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPastDelinquencies;
+        
+        /// <summary>
+        /// txtCreditAgeDays control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCreditAgeDays;
+        
+        /// <summary>
+        /// txtFICO control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFICO;
+        
+        /// <summary>
+        /// txtInquiries6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtInquiries6;
+        
+        /// <summary>
+        /// txtLastDelinquency control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLastDelinquency;
+        
+        /// <summary>
+        /// txtOpenAccounts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtOpenAccounts;
+        
+        /// <summary>
+        /// txtDerogatoryRecords control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDerogatoryRecords;
+        
+        /// <summary>
+        /// txtTotalCreditBalance control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTotalCreditBalance;
+        
+        /// <summary>
+        /// txtCreditUtil control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCreditUtil;
+        
+        /// <summary>
+        /// txtTotAccounts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTotAccounts;
+        
+        /// <summary>
+        /// txtPastCollections12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPastCollections12;
+        
+        /// <summary>
+        /// txtDelinquentAmount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDelinquentAmount;
+        
+        /// <summary>
+        /// txtTotCurBalance control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTotCurBalance;
+        
+        /// <summary>
+        /// txtRevTrades24 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRevTrades24;
+        
+        /// <summary>
+        /// txtBalanceToLimit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBalanceToLimit;
+        
+        /// <summary>
+        /// txtInquiries12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtInquiries12;
+        
+        /// <summary>
+        /// txtAccountsOpened24 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAccountsOpened24;
+        
+        /// <summary>
+        /// txtChargeOffs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtChargeOffs;
+        
+        /// <summary>
+        /// txtPastDueAmount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPastDueAmount;
+        
+        /// <summary>
+        /// txtMortgage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList txtMortgage;
+        
+        /// <summary>
+        /// txtBankruptcies control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList txtBankruptcies;
+        
+        /// <summary>
+        /// txtTaxLiens control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList txtTaxLiens;
+        
+        /// <summary>
         /// lbPredict control.
         /// </summary>
         /// <remarks>
